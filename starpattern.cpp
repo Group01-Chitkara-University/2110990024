3131e50cf13799cc62ddb56e4ae38a0119a37aab
Aastha Anand <95748206+aasthaanand09@users.noreply.github.com>
2022-05-24 22:41:29 +0530

    Conditionals and Loops solution

---
+++ starpattern.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+       /*  Read input as specified in the question.
+	* Print output as specified in the question.
+	*/
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        int gap=n-i;
+        for(int j=1;j<=gap;j++){
+            cout<<" ";
+        }
+        for(int k=1;k<=i;k++){
+            cout<<"*";
+        }
+        for(int t=1;t<=i-1;t++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+  
+}
